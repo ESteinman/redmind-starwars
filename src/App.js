8ba1b7aea055428efb93bd9f93b152f074f7e91e
ESteinman <erik.steinman@hotmail.com>
2021-08-26 14:14:59 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
 import React, {Component} from "react";
-import Axios from 'axios';
 import './App.css';
 import axios from 'axios';
-import { render } from '@testing-library/react';
 
 
 export default class App extends Component {
@@ -57,39 +54,3 @@ export default class App extends Component {
     )
   }
 }
-// function App() {
-//   const [people, setPeople] = useState("");
-
-
-
-
-//   function getResponse() {
-//     Axios.get("https://swapi.dev/api/people").then(function(response) {
-//       console.log(response);
-//       showNames(response.data);
-//     })
-//   }
-
-//     function showNames(data) {
-//       var names = ""
-//       for (var i = 0; i < data.results.length; i++) {
-//         names = names + data.results[i].name + "\n";
-//       }
-//         if (data.next) {
-//           getResponse(data.next)
-//         }
-//         else {
-//           console.log(names)
-//         }
-//     }
-
-//   return (
-    
-//     <div>
-//       Hello <button onClick={getResponse}>Get Star Wars characters</button>
-//     {names}
-//     </div>
-//   );
-// }
-
-// export default App;
